4489e4db19301ecf0c6fb448a61121f1bee5c130
Tenkai Ruri <babichan@outlook.com>
2019-04-11 18:59:29 +0800

    docs：update license

--- test/flutter_video_compress_test.dart
+++ test/flutter_video_compress_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_video_compress/flutter_video_compress.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter_video_compress/flutter_video_compress.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flutter_video_compress');
