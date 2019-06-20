80f88b84d64feee733f8aef0d506bd88ee3635ce
Gustavo Contreiras <guto.contreiras@gmail.com>
2019-06-20 01:41:42 -0300

    A better example app
    Pick video from gallery or camera and then see the results on screen or on console.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 import 'dart:typed_data';
 
@@ -13,17 +14,24 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
   final _flutterVideoCompress = FlutterVideoCompress();
-  Uint8List _image;
-  File _imageFile;
   Subscription _subscription;
+
+  Image _thumbnailUint8ListImage;
+  Image _thumbnailFileImage;
+  Image _gifFileImage;
+
+  MediaInfo _originalVideoInfo = MediaInfo(path: '');
+  MediaInfo _compressedVideoInfo = MediaInfo(path: '');
+
+  StreamController<bool> _loadingStreamCtrl = StreamController<bool>.broadcast();
 
   @override
   void initState() {
     super.initState();
-    _subscription =
-        _flutterVideoCompress.compressProgress$.subscribe((progress) {
-      print('progress: $progress');
+    _subscription = _flutterVideoCompress.compressProgress$.subscribe((progress) {
+      //debugPrint('[Compressing Progress] $progress %');
     });
   }
 
@@ -31,117 +39,151 @@ class _MyAppState extends State<MyApp> {
   void dispose() {
     super.dispose();
     _subscription.unsubscribe();
+    _loadingStreamCtrl.close();
   }
 
-  Future<void> _videoPicker() async {
-    if (mounted) {
-      final file = await ImagePicker.pickVideo(source: ImageSource.camera);
-      if (file?.path != null) {
-        final thumbnail = await _flutterVideoCompress.getThumbnail(
-          file.path,
-          quality: 50,
-          position: -1,
-        );
+  Future<void> runFlutterVideoCompressMethods(BuildContext context, File videoFile) async {
 
-        setState(() {
-          _image = thumbnail;
+    _loadingStreamCtrl.sink.add(true);
+
+    var _startDateTime = DateTime.now();
+    print('[Compressing Video] start');
+    await _flutterVideoCompress.startCompress(videoFile.path, quality: VideoQuality.DefaultQuality, deleteOrigin: false).then((MediaInfo compressedVideoInfo) async {
+      print('[Compressing Video] done! ${DateTime.now().difference(_startDateTime).inSeconds}s');
+
+      _startDateTime = DateTime.now();
+      print('[Getting Thumbnail BytesList] start');
+      await _flutterVideoCompress.getThumbnail(videoFile.path, quality: 50).then((Uint8List thumbnailUint8ListImage) async {
+        print('[Getting Thumbnail BytesList] done! ${DateTime.now().difference(_startDateTime).inSeconds}s');
+
+        _startDateTime = DateTime.now();
+        print('[Getting Thumbnail File] start');
+        await _flutterVideoCompress.getThumbnailWithFile(videoFile.path, quality: 50).then((File thumbnailFile) async {
+          print('[Getting Thumbnail File] done! ${DateTime.now().difference(_startDateTime).inSeconds}s');
+
+          _startDateTime = DateTime.now();
+          print('[Getting Gif File] start');
+          await _flutterVideoCompress.convertVideoToGif(videoFile.path, startTime: 0, endTime: 5).then((File gifFile) async {
+            print('[Getting Gif File] done! ${DateTime.now().difference(_startDateTime).inSeconds}s');
+
+            await _flutterVideoCompress.getMediaInfo(videoFile.path).then((MediaInfo videoInfo) async {
+
+              setState(() { 
+                _thumbnailUint8ListImage = Image.memory(thumbnailUint8ListImage);
+                _thumbnailFileImage = Image.file(thumbnailFile);
+                _gifFileImage = Image.file(gifFile);
+                _originalVideoInfo = videoInfo;
+                _compressedVideoInfo = compressedVideoInfo; 
+              });
+            });
+
+          });
         });
+      });
+    });
 
-        final resultFile = await _flutterVideoCompress.getThumbnailWithFile(
-          file.path,
-          quality: 50,
-          position: -1,
-        );
-        print(resultFile.path);
-
-        assert(resultFile.existsSync());
-
-        print('file Exists: ${resultFile.existsSync()}');
-
-        final MediaInfo info = await _flutterVideoCompress.startCompress(
-          file.path,
-          deleteOrigin: true,
-          quality: VideoQuality.LowQuality,
-        );
-        print(info.toJson());
-      }
-    }
-  }
-
-  Future<void> _stopCompress() async {
-    await _flutterVideoCompress.stopCompress();
-  }
-
-  Future<void> _getMediaInfo() async {
-    if (mounted) {
-      final file = await ImagePicker.pickVideo(source: ImageSource.gallery);
-      if (file?.path != null) {
-        final info = await _flutterVideoCompress.getMediaInfo(file.path);
-        print(info.toJson());
-      }
-    }
-  }
-
-  Future<void> _convertVideoToGif() async {
-    if (mounted) {
-      final file = await ImagePicker.pickVideo(source: ImageSource.gallery);
-      if (file?.path != null) {
-        final info = await _flutterVideoCompress.convertVideoToGif(
-          file.path,
-          startTime: 0,
-          duration: 5,
-        );
-
-        print(info.path);
-        setState(() {
-          _imageFile = info;
-        });
-      }
-    }
-  }
-
-  List<Widget> _builColumnChildren() {
-    // dart 2.3 before
-    // final _list = <Widget>[
-    // FlatButton(child: Text('take video'), onPressed: _videoPicker),
-    // FlatButton(child: Text('stop compress'), onPressed: _stopCompress),
-    // FlatButton(child: Text('getMediaInfo'), onPressed: _getMediaInfo),
-    // FlatButton(
-    //   child: Text('convert video to gif'),
-    //   onPressed: _convertVideoToGif,
-    // ),
-    // ];
-    // if (_image != null) {
-    //   _list.add(Flexible(child: Image.memory(_image)));
-    // }
-    // return _list;
-
-    // dart 2.3
-    final _list = [
-      FlatButton(child: Text('take video'), onPressed: _videoPicker),
-      FlatButton(child: Text('stop compress'), onPressed: _stopCompress),
-      FlatButton(child: Text('get media info'), onPressed: _getMediaInfo),
-      FlatButton(
-        child: Text('convert video to gif'),
-        onPressed: _convertVideoToGif,
-      ),
-      if (_imageFile != null)
-        Flexible(child: Image.file(_imageFile))
-      else
-        if (_image != null) Flexible(child: Image.memory(_image))
-    ];
-    return _list;
+    _loadingStreamCtrl.sink.add(false);
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('Plugin example app')),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: _builColumnChildren(),
-        ),
+        appBar: AppBar(title: const Text('Flutter Video Compress Example')),
+        body: Stack(
+          children: <Widget>[
+            ListView(
+              children: <Widget>[
+
+                Padding(padding: EdgeInsets.only(top: 20),),
+
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 32),
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+                    child: Text("Take video from camera with Image Picker", style: TextStyle(color: Colors.white),),
+                    color: Colors.grey[800],
+                    onPressed: () async {
+                      await ImagePicker.pickVideo(source: ImageSource.camera).then((File videoFile) async {
+                        runFlutterVideoCompressMethods(context, videoFile);                  
+                      });
+                    },
+                  ),
+                ),
+
+                Container(
+                  padding: EdgeInsets.only(left: 32, right: 32, top: 8, bottom: 8),
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+                    child: Text("Take video from gallery with Image Picker", style: TextStyle(color: Colors.white),),
+                    color: Colors.grey[800],
+                    onPressed: () async {
+                      await ImagePicker.pickVideo(source: ImageSource.gallery).then((File videoFile) async {
+                        runFlutterVideoCompressMethods(context, videoFile);
+                      });
+                    },
+                  ),
+                ),
+
+                Container(alignment: Alignment.center, padding: EdgeInsets.only(left: 8, right: 8, top: 8), child: Text('Original video')),
+
+                Container(alignment: Alignment.centerLeft, padding: EdgeInsets.all(8.0), 
+                  child: Text("path: ${_originalVideoInfo.path}\nduration: ${_originalVideoInfo.duration} microseconds\nsize: ${_originalVideoInfo.filesize} bytes\nsize: ${_originalVideoInfo.width} x ${_originalVideoInfo.height}\ncompression cancelled: ${_originalVideoInfo.isCancel}\nauthor: ${_originalVideoInfo.author}")
+                ),
+
+                Divider(),
+
+                Container(alignment: Alignment.center, padding: EdgeInsets.only(left: 8, right: 8, top: 8), child: Text('Compressed video')),
+
+                Container(alignment: Alignment.centerLeft, padding: EdgeInsets.all(8.0), 
+                  child: Text("path: ${_compressedVideoInfo.path}\nduration: ${_compressedVideoInfo.duration} microseconds\nsize: ${_compressedVideoInfo.filesize} bytes\nsize: ${_compressedVideoInfo.width} x ${_compressedVideoInfo.height}\ncompression cancelled: ${_originalVideoInfo.isCancel}\nauthor: ${_originalVideoInfo.author}")
+                ),
+
+                Divider(),
+
+                Container(alignment: Alignment.center, padding: EdgeInsets.only(left: 8, right: 8, top: 8), child: Text('Thumbnail image from file')),
+
+                _thumbnailFileImage != null ? _thumbnailFileImage : Container(),
+
+                Divider(),
+
+                Container(alignment: Alignment.center, padding: EdgeInsets.only(left: 8, right: 8, top: 8), child: Text('Thumbnail image from bytes list')),
+                
+                _thumbnailUint8ListImage != null ? _thumbnailUint8ListImage : Container(),
+
+                Divider(),
+
+                Container(alignment: Alignment.center, padding: EdgeInsets.only(left: 8, right: 8, top: 8), child: Text('Gif image from original video')),
+
+                _gifFileImage != null ? _gifFileImage : Container(),
+
+                Divider(),
+
+              ],
+            ),
+          
+            StreamBuilder<bool>(
+              stream: _loadingStreamCtrl.stream,
+              builder: (context, AsyncSnapshot<bool> snapshot) {
+                if (snapshot.data == true) {
+                  return Container(
+                    color: Colors.black54,
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        CircularProgressIndicator(), 
+                        Container(padding: EdgeInsets.all(8), child: Text('Compressing...'))
+                      ]
+                    ),
+                  );
+                } return Container();
+              }
+            )
+          ],
+        )
       ),
     );
   }
